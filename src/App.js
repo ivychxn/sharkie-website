81681ae8fef3132719b7fcff687628fb1ab999ab
ivychxn <ivy@acmucsd.org>
2023-11-04 00:35:53 -0700

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Wishlist from "./routes/Wishlist";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="home" element={<Home />} />
+          <Route path="wishlist" element={<Wishlist />} />
+        </Routes>
+
+        <Link to="home">Home</Link><br></br>
+        <Link to="wishlist">Wishlist</Link>
+
+      </div>
+    </Router>
   );
 }
-
-export default App;
