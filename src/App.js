e0e71209060b3496704fbdb76b5bd50adbaf3ccf
ivychxn <ivy@acmucsd.org>
2024-01-28 19:07:28 -0800

    basic styling and stuff added yk

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import Home from "./routes/Home";
 import Wishlist from "./routes/Wishlist";
+import Template from "./routes/Template";
 
 export default function App() {
   return (
@@ -15,10 +16,12 @@ export default function App() {
         <Routes>
           <Route path="home" element={<Home />} />
           <Route path="wishlist" element={<Wishlist />} />
+          <Route path="template" element={<Template />} />
         </Routes>
 
         <Link to="home">Home</Link><br></br>
         <Link to="wishlist">Wishlist</Link>
+        <Link to="template">Template</Link>
 
       </div>
     </Router>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "../styles/main.css"
 
 const Home = () => {
-    return <h1>Home test</h1>;
-  };
-  
+  return (
+    <div>
+      <div className='wave'/>
+      <div className="container">
+        <div className='title-only'>
+          <h3>hi. i like sharks.</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Home
